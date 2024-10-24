f6bcd1edee65d50c80e3685b769643694c88f3d5
svensto190 <35144055+svensto190@users.noreply.github.com>
2024-10-24 12:36:47 +0200

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,5 +1,4 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-// set to 0 to enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define QUICK_TAP_TERM 100
+
